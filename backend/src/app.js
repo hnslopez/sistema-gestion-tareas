21838715cf89aaebc36198590c8309949cb18ae7
RE <hans.lopezaranda@gmail.com>
2023-01-31 22:45:33 -0300

    primer commit
    cree  las carpeta src  con los algunas subcarpetas e inicie express.

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+const app = express();
+const routes = require('./routes');
+
+
+// Use cors middleware to allow cross-origin requests
+app.use(cors());
+
+// Use morgan for request logging
+app.use(morgan("tiny"));
+
+// Use body-parser middleware to parse request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(routes);
+
+app.post("/api/example", (req, res) => {
+  res.send({
+    success: true,
+    message: "Example route",
+    data: req.body
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
